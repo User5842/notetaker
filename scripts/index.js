30c8d6fb2d208cf3721d5b58a55a527389927827
Rafael Negron <r.negron1@umiami.edu>
2021-03-21 13:40:08 -0400

    Refactoring and adding dynamic notes

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,17 @@
 import { Note } from './note.js';
 
+// Wrappers
 const notesWrapper = document.querySelector('.notes');
 
-const note = new Note('First note', 'First note message');
+// Form elements
+const noteForm = document.forms['form'];
 
-notesWrapper.append(note.generateNote());
+noteForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const title = noteForm.elements['title'].value;
+    const message = noteForm.elements['message'].value;
+
+    const note = new Note(title, message);
+    notesWrapper.append(note.generateNote());
+});
